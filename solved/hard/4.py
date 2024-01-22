2e604bca4d4d01103c067a4aac81b5d805486839
renanmoretto <himynameisrenan@outlook.com>
2024-01-22 11:02:22 -0300

    4 solved

--- solved/hard/4.py
+++ solved/hard/4.py
@@ -7,16 +7,18 @@ The overall run time complexity should be O(log (m+n)).
 """
 
 
+# This is faster then using statistics median, idk why
 def solution(nums1: list[int], nums2: list[int]) -> float:
-    nums = nums1 + nums2
-    nums.sort()
-
+    nums = sorted(nums1 + nums2)
     size = len(nums)
-    if size % 2 == 0:
-        n1 = nums[size // 2 - 1]
-        n2 = nums[size // 2]
-        return (n1 + n2) / 2
-    return nums[size // 2]
+    return nums[size // 2] if size % 2 else (nums[size // 2 - 1] + nums[size // 2]) / 2
+
+
+from statistics import median
+
+
+def solution2(nums1: list[int], nums2: list[int]) -> float:
+    return median(sorted(nums1 + nums2))
 
 
 if __name__ == '__main__':
